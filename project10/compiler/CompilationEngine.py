a8de1f39dd5ae92bc76be8a99cde937c66fc7205
bgx <bgx@users.noreply.github.com>
2015-04-02 18:48:32 -0400

    Add: Function "declarations" added to CompEngine

--- project10/compiler/CompilationEngine.py
+++ project10/compiler/CompilationEngine.py
@@ -1,7 +1,10 @@
 ''' CompilationEngine '''
 
 def compile(tokens,output):
-    '''test output if tokens are being passed correctly'''    
+    '''test output if tokens are being passed correctly'''  
+    
+    n = len(tokens)
+    
     for pair in tokens:
         xml = pair[0] + ' ' + str(pair[1])
         if not pair:
@@ -10,6 +13,36 @@ def compile(tokens,output):
             oline = '\t' + xml + '\n'
         output.write(oline)
         
+def getToken(n):
+
+def compileClass(output):
+
+def compileClassVarDec():
+
+def compileSubroutine():
+
+def compileParameterList():
+
+def compileVarDec():
+
+def compileStatements():
+
+def compileDo():
+
+def compileLet():
+
+def compileWhile():
+
+def compileReturn():
+
+def compileIf():
+
+def compileExpression():
+
+def compileTerm():
+
+def compileExpressionList():
+      
 
 if __name__ == '__main__':
     sys.exit(main())
