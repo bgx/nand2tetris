f1227715f69d0c5f7655cbd7656ee68b5f35e780
Brian Grady <bgx@users.noreply.github.com>
2015-12-06 19:52:08 -0500

    Ref: Reorganized project03
    Reorganized project03 to separate authored chip designs from all the other
    files used to test the chip designs.
    
    Chips folder contains my chip designs
    TestFiles contains the supplied test scripts and comparison files
    Workspace is where .hdl, .tst, and .cmp files can be copied to for testing
    
    Batch files automate copying files to and deleting files from the
    Workspace
    
    Added a shortcut to the HardwareSimulator
    
    Added information to the README file

---
+++ project03/CopyToWorkspace_Chips-a.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\Brian\Documents\nand2tetris\src\project03\Workspace"
+copy "C:\Users\Brian\Documents\nand2tetris\src\project03\Chips\a\*.hdl"
---
+++ project03/CopyToWorkspace_TestFiles.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\Brian\Documents\nand2tetris\src\project03\Workspace"
+copy "C:\Users\Brian\Documents\nand2tetris\src\project03\TestFiles\a\*.cmp"
+copy "C:\Users\Brian\Documents\nand2tetris\src\project03\TestFiles\a\*.tst"
+copy "C:\Users\Brian\Documents\nand2tetris\src\project03\TestFiles\b\*.cmp"
+copy "C:\Users\Brian\Documents\nand2tetris\src\project03\TestFiles\b\*.tst"
